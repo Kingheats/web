a61a3d640acf78e39ee36ec64c71228ec6528e53
Kingheats <86288467+Kingheats@users.noreply.github.com>
2021-07-30 17:13:43 +0800

    insert 2021-07-30--04  internet

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 2222222222222666666666595
 // test update 20210623-03
 
-3333333
+333
 // i have a new delete 2021-07-30--03  internet 
 //test update 2021-07-30
-
+ new delete 2021-07-30--04  internet 
 
 // i have a new insert 2021-07-30...
 
@@ -21,3 +21,6 @@
 
 // now i have a new insert 2021-07-30--02
 // new insert 2021-07-30-03--internet
+
+ 
+  new insert 2021-07-30--04  internet
