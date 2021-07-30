7aeb3d420b3fe907152bcf7f4b1e0530988234f3
Kingheats <Kingheat@qq.com>
2021-07-30 17:15:11 +0800

    newinsert-03

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 111111111
-2222222222222666666666595
+// 2222222   new delete 2021-07-30--03 offine
 // test update 20210623-03
 
-3333333
+333333333333333333333333
 // i have a new delete 2021-07-30--03  internet 
 //test update 2021-07-30
-
+// new insert 2021-07-30--03 offine
 
 // i have a new insert 2021-07-30...
 
@@ -21,3 +21,5 @@
 
 // now i have a new insert 2021-07-30--02
 // new insert 2021-07-30-03--internet
+
+
